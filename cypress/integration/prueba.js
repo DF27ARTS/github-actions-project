d0622beca5c2f889ba3a6b82286faddbdcf3bf02
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2024-01-02 13:27:54 -0500

    Add cypress testing

---
+++ cypress/integration/prueba.js
@@ -0,0 +1,8 @@
+/* global cy */
+
+describe("Prueba Básica de Cypress", () => {
+  it("Debe ejecutar la prueba correctamente", () => {
+    // Realiza una aserción simple
+    expect(true).to.equal(true);
+  });
+});
