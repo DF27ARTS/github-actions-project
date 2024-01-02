9e774e5b749c26ea0919bd60605cf95d060c1e0a
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2024-01-02 15:50:59 -0500

    Eliminar prueba de testing de prueba

--- cypress/integration/prueba.js
+++
@@ -1,8 +0,0 @@
-/* global cy */
-
-describe("Prueba Básica de Cypress", () => {
-  it("Debe ejecutar la prueba correctamente", () => {
-    // Realiza una aserción simple
-    expect(true).to.equal(true);
-  });
-});
